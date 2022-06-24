1a3cc5e4cf1516c539588e46571a2b4c5a67481f
Alex Wasinger <awasing2@nd.edu>
2022-06-24 16:53:23 -0500

    gen compile commands

--- src/main.cpp
+++ src/main.cpp
@@ -25,7 +25,7 @@ int main() {
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
     glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
-    
+
     GLFWwindow *window = glfwCreateWindow(800, 600, "LearnOpenGL", NULL, NULL);
     if (!window) {
         std::cerr << "Failed to create GLFW window" << std::endl;
