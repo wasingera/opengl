511ed094add8ecfa7cb2d5e40d4c53092c813201
Alex Wasinger <awasing2@nd.edu>
2022-06-27 12:22:58 -0500

    coordinates lesson complete

--- include/shader.h
+++ include/shader.h
@@ -6,6 +6,8 @@
 #include <fstream>
 #include <sstream>
 #include <iostream>
+#include <glm/glm.hpp>
+#include <glm/gtc/type_ptr.hpp>
 
 class Shader {
     public:
@@ -19,6 +21,7 @@ class Shader {
     void setBool(const std::string &name, bool value) const;
     void setInt(const std::string &name, int value) const;
     void setFloat(const std::string &name, float value) const;
+    void setMat4(const std::string &name, glm::mat4 &m) const;
 };
 
 #endif
